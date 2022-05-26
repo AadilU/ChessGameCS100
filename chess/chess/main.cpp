8c5687b8b56aeede20a2b323723b799f2d5ab0fa
Angel Ibanez <angelibanez96@gmail.com>
2022-05-26 09:57:06 -0700

    testing

---
+++ chess/chess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chess
+//
+//  Created by Angel Ibanez on 5/26/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
