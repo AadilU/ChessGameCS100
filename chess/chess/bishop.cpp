27879f2ba8f6469834f95432bd8aa2da66761c54
AadilU <aadilu2002@gmail.com>
2022-06-02 01:38:47 -0700

    Fully functional bishop

--- chess/chess/bishop.cpp
+++ chess/chess/bishop.cpp
@@ -22,6 +22,128 @@ bishop::bishop(bool w,int pX, int pY){
 }
 
 std::vector<std::pair<int, int>> bishop::possibleMoves(bool w, Game* game) {
+    int x = posX;
+    int y = posY;
+
     std::vector<std::pair<int, int>> moves;
+
+        int indexOfYUpEnemy = 0;
+        bool YUpPieceFound = false;
+        for(int i = y - 1;i >= 0;i--) {
+            if(!YUpPieceFound) {
+                x++;
+                if(x == 7) {
+                    YUpPieceFound = true;
+                    indexOfYUpEnemy = i;
+                }
+                if(game->getAttackedPiece(x, i) != nullptr) {
+                    YUpPieceFound = true;
+                    if(game->getAttackedPiece(x, i)->isWhite() == w) {
+                        indexOfYUpEnemy = i + 1;
+                        x = x - 1;
+                    }
+                    else
+                        indexOfYUpEnemy = i;
+                }
+            }
+        }
+
+        x = posX;
+
+        for(int i = y;i >= indexOfYUpEnemy;i--) {
+            moves.push_back(std::make_pair(x, i));
+            x++;
+        }
+
+        x = posX;
+
+        indexOfYUpEnemy = 0;
+        YUpPieceFound = false;
+        for(int i = y - 1;i >= 0;i--) {
+            if(!YUpPieceFound) {
+                x--;
+                if(x == 0) {
+                    YUpPieceFound = true;
+                    indexOfYUpEnemy = i;
+                }
+                if(game->getAttackedPiece(x, i) != nullptr) {
+                    YUpPieceFound = true;
+                    if(game->getAttackedPiece(x, i)->isWhite() == w) {
+                        indexOfYUpEnemy = i + 1;
+                        x = x + 1;
+                    }
+                    else
+                        indexOfYUpEnemy = i;
+                }
+            }
+        }
+
+        x = posX;
+
+        for(int i = y;i >= indexOfYUpEnemy;i--) {
+            moves.push_back(std::make_pair(x, i));
+            x--;
+        }
+
+        x = posX;
+
+        int indexOfYDownEnemy = 7;
+        bool YDownPieceFound = false;
+        for(int i = y + 1;i <= 7;i++) {
+            if(!YDownPieceFound)
+                x++;
+                if(x == 7) {
+                    YDownPieceFound = true;
+                    indexOfYDownEnemy = i;
+                }
+                if(game->getAttackedPiece(x, i) != nullptr) {
+                    YDownPieceFound = true;
+                    if(game->getAttackedPiece(x, i)->isWhite() == w) {
+                        indexOfYDownEnemy = i - 1;
+                        x = x - 1;
+                    }
+                    else
+                        indexOfYDownEnemy = i;
+                }
+        }
+
+        x = posX;
+
+        for(int i = y;i <= indexOfYDownEnemy;i++) {
+            moves.push_back(std::make_pair(x, i));
+            x++;
+        }
+
+        x = posX;
+
+        indexOfYDownEnemy = 7;
+        YDownPieceFound = false;
+        for(int i = y + 1;i <= 7;i++) {
+            if(!YDownPieceFound)
+                x--;
+                if(x == 0) {
+                    YDownPieceFound = true;
+                    indexOfYDownEnemy = i;
+                }
+                if(game->getAttackedPiece(x, i) != nullptr) {
+                    YDownPieceFound = true;
+                    if(game->getAttackedPiece(x, i)->isWhite() == w) {
+                        indexOfYDownEnemy = i - 1;
+                        x = x + 1;
+                    }
+                    else
+                        indexOfYDownEnemy = i;
+                }
+        }
+
+        x = posX;
+
+        for(int i = y;i <= indexOfYDownEnemy;i++) {
+            moves.push_back(std::make_pair(x, i));
+            x--;
+        }
+
+        x = posX;
+
     return moves;
 }
